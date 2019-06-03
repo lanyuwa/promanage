b16dc2f6d54a13b1c72d0317eb1cb35826ca967d
unknown <you@example.com>
2019-06-03 22:52:43 +0800

    第二次提交

---
+++ src/components/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h5>memeda</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
